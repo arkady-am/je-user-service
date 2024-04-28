ecfc0e65b04b1af7aa377c34eea7550281dfe56f
Arkady <git@arkady.de>
2024-04-28 14:34:33 +0200

    First Version

---
+++ src/main/java/com/jesse/treffpunktservice/TreffpunktServiceApplication.java
@@ -0,0 +1,13 @@
+package com.jesse.treffpunktservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreffpunktServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreffpunktServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jesse/treffpunktservice/TreffpunktServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jesse.treffpunktservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreffpunktServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
